435037bcb98233c82566a5bbd76e3840ad0239e6
Inklud AS <61893998+Inklud@users.noreply.github.com>
2020-04-10 16:08:49 +0200

    Initial commit

---
+++ src/pages/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1 class="text-center">About this project</h1>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
