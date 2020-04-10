f2f9000a7d9a90c3ee9e178ba42bfbe97da3dd3e
Inklud AS <61893998+Inklud@users.noreply.github.com>
2020-04-10 21:26:50 +0200

    Updated project

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <h1 class="text-center">About this project!</h1>
+    <h1 class="text-center">About this project</h1>
   </q-page>
 </template>
 
