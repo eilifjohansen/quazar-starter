9a29419212825f6ed576b7cea74973c12bf42a25
Inklud AS <61893998+Inklud@users.noreply.github.com>
2020-04-11 19:51:40 +0200

    Added Covidu avatar

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -8,7 +8,14 @@
         :name="message.from"
         :text="[message.text]"
         :sent="message.from == 'Me' ? true : false"
-      />
+      >
+        <template v-slot:avatar v-if="message.from == 'Covidu' ? true : false">
+          <img
+            class="q-message-avatar q-message-avatar--sent"
+            src="https://scontent.fosl3-1.fna.fbcdn.net/v/t1.0-9/89266455_112961590318562_634611096208015360_n.jpg?_nc_cat=108&_nc_sid=85a577&_nc_ohc=HC3VlJYOkZEAX_dcpJY&_nc_ht=scontent.fosl3-1.fna&oh=6d32603581bb75296f13397538d814ed&oe=5EB72364"
+          >
+        </template>
+      </q-chat-message>
     </div>
     <q-footer elevated>
       <q-toolbar>
